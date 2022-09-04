18f9e33f1d1c33e08e1a37e5b6c2e35449280cc8
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-09-05 00:26:12 +0900

    Update

--- src/BackupCommand.php
+++ src/BackupCommand.php
@@ -5,19 +5,33 @@ declare(strict_types=1);
 namespace outiserver\backup;
 
 use CortexPE\Commando\BaseCommand;
+use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\lang\Translatable;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginOwned;
 use pocketmine\Server;
 
-class BackupCommand extends BaseCommand
+class BackupCommand extends Command implements PluginOwned
 {
-    protected function prepare(): void
+    private Plugin $plugin;
+
+    public function __construct(Plugin $plugin, string $name, Translatable|string $description = "", Translatable|string|null $usageMessage = null, array $aliases = [])
     {
+        parent::__construct($name, $description, $usageMessage, $aliases);
+
+        $this->plugin = $plugin;
         $this->setPermission("backup.command");
     }
 
-    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
+    public function getOwningPlugin(): Plugin
+    {
+        return  $this->plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
         $sender->sendMessage("バックアップを開始しています...");
-        Server::getInstance()->getAsyncPool()->submitTask(new ZipBackupAsyncTask(Backup::getInstance()->getDataFolder(), Server::getInstance()->getFilePath()));
+        Server::getInstance()->getAsyncPool()->submitTask(new ZipBackupAsyncTask(Backup::getInstance()->getDataFolder(), Server::getInstance()->getDataPath()));
     }
 }
